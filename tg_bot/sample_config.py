d6e3635c615b106416b46e0c5061b0af1fa28b56
TGExplore <52099763+TGExplore@users.noreply.github.com>
2019-09-05 22:40:53 +0530

    Update sample_config.py

--- tg_bot/sample_config.py
+++ tg_bot/sample_config.py
@@ -11,8 +11,8 @@ class Config(object):
 
     # REQUIRED
     API_KEY = "YOUR KEY HERE"
-    OWNER_ID = "YOUR ID HERE"  # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "YOUR USERNAME HERE"
+    OWNER_ID = "683538773"  # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_USERNAME = "ViruZs"
 
     # RECOMMENDED
     SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
